46a6a5e311f41765b93e03d862f59fa2cc98ab86
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-01-26 10:53:32 +0530

    Operator Class: Peri of square.

---
+++ praticeProblem/jspider class ass/src/operatos/perimeterOfSquare.java
@@ -0,0 +1,9 @@
+package operatos;
+
+public class perimeterOfSquare {
+    public static void main(String[] args) {
+        int s = 5;
+        int peri = 4*s;
+        System.out.println("Perimeter of square is: "+ peri);
+    }
+}
