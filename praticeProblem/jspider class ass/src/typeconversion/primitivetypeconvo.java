4ad5fb46d1466df23dd2136a168333b6dd993197
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-01-29 16:01:41 +0530

    primitive typecasting

---
+++ praticeProblem/jspider class ass/src/typeconversion/primitivetypeconvo.java
@@ -0,0 +1,21 @@
+package typeconversion;
+
+public class primitivetypeconvo {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int a = ch;
+        System.out.println(ch);
+        System.out.println(a);
+
+
+        int x = 20;
+        int b = 30;
+        double c = x+b;
+        System.out.println(c);
+
+        int h = 10;
+        double i = 10;
+        double k = (h + i);
+        System.out.println(h+" + "+i+" = "+k);
+    }
+}
