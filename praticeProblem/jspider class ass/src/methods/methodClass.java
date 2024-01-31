3b3909f098939f3f5c16a18b320ec2a3ae103ea6
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-01-31 16:16:20 +0530

    Method calling and declaration of a method

---
+++ praticeProblem/jspider class ass/src/methods/methodClass.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class methodClass {
+    public static void main(String[] args) {
+        System.out.println("Main starts...");
+        System.out.println("Main ends....");
+        demo(); //method calling
+    }
+    public static void demo(){ //method declaration
+        System.out.println("Hello!!");
+    }
+}
