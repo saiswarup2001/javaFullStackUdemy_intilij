29e47f4bb60ff578318945153341fd04bf1bad67
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-02-01 14:50:23 +0530

    Receive char value and return its ascii value

---
+++ praticeProblem/jspider class ass/src/methods/questions/asciiValue.java
@@ -0,0 +1,14 @@
+package methods.questions;
+
+public class asciiValue {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int aValue = AsValue(ch);
+        System.out.println("ASCII Value of "+ ch +" is = "+ aValue);
+    }
+
+    public  static int AsValue(char c){
+        int k = c;
+        return k;
+    }
+}
