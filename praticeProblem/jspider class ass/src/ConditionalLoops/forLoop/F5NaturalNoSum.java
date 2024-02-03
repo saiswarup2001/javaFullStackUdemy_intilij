7346db468932c4393b9ff94ab11f2ec45edd5b05
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-02-03 15:43:43 +0530

    for loop program: print the first 10 natural number sum?

---
+++ praticeProblem/jspider class ass/src/ConditionalLoops/forLoop/F5NaturalNoSum.java
@@ -0,0 +1,12 @@
+package ConditionalLoops.forLoop;
+
+public class F5NaturalNoSum {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i=1; i<=10; i++){
+            sum = sum + i;
+            System.out.println(sum);
+        }
+        //System.out.println(sum);
+    }
+}
