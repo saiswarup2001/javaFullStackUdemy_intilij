3d33f51a227979f5bd0b7b63b6d34f6e7ace8722
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-02-03 14:55:42 +0530

    while loop program: print 1 to 5

---
+++ praticeProblem/jspider class ass/src/ConditionalLoops/whileLoop/P1.java
@@ -0,0 +1,20 @@
+package ConditionalLoops.whileLoop;
+
+public class P1 {
+    public static void main(String[] args) {
+        int a = 1;
+        /*
+        to print star for 5times
+        while(a<=5){
+            System.out.println("*");
+            a++;
+        }
+         */
+
+        // to print 1 to 5
+        while(a<=5){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
