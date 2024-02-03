12e9c797835a2be83d68374c7a1120a89d284e55
Sai Swarup Patnaik <saiswarup2001@gmail.com>
2024-02-03 14:54:24 +0530

    switch case

---
+++ praticeProblem/jspider class ass/src/ConditionalLoops/SwitchCase/p1.java
@@ -0,0 +1,19 @@
+package ConditionalLoops.SwitchCase;
+
+public class p1 {
+    public static void main(String[] args) {
+        int a = 1;
+
+        switch (a){
+            case 1:
+                System.out.println("Hi...");
+                break;
+            case 2:
+                System.out.println("Hello...");
+                break;
+            case 3:
+                System.out.println("Bye....");
+                break;
+        }
+    }
+}
