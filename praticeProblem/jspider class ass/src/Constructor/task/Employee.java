dea1a5f52f4acd99da8f80e1d3056f5cad77b685
saiswarup2001 <saiswarup2001@gmail.com>
2024-02-12 15:56:23 +0530

    this keyword and problem practice

---
+++ praticeProblem/jspider class ass/src/Constructor/task/Employee.java
@@ -0,0 +1,15 @@
+package Constructor.task;
+
+public class Employee {
+    int eid;
+    String eName;
+
+    Employee(String eName, int eid){
+        this.eid = eid;
+        this.eName = eName;
+    }
+
+    void display(){
+        System.out.println("Emp Id: "+ eid +" Emp Name is: "+ eName);
+    }
+}
---
+++ praticeProblem/jspider class ass/src/Constructor/task/EmployeeRun.java
@@ -0,0 +1,10 @@
+package Constructor.task;
+
+public class EmployeeRun {
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Sheela", 1);
+        Employee e2 = new Employee("Allem", 2);
+        e1.display();
+        e2.display();
+    }
+}
