e3c8685b335546eeb737c982930e4b1a58954200
saiswarup2001 <saiswarup2001@gmail.com>
2024-02-15 14:41:58 +0530

    Static Variable by defaults take 0 in case of int.

---
+++ praticeProblem/jspider class ass/src/staticAndNonStatic/Demo.java
@@ -0,0 +1,11 @@
+package staticAndNonStatic;
+
+public class Demo {
+    static int a,b,c;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
