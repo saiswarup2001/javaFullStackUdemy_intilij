0dc49d7efda0a3e32cf0320c1e4b02da83366549
saiswarup2001 <saiswarup2001@gmail.com>
2024-02-24 10:35:52 +0530

    Generics Implementation

---
+++ Generics/src/Generic.java
@@ -0,0 +1,34 @@
+
+class MyGen<type>{
+    type t ;
+    String name;
+
+    public MyGen(type t, String name) {
+        this.t = t;
+        this.name = name;
+    }
+
+    public type getT() {
+        return t;
+    }
+
+    public void setT(type t) {
+        this.t = t;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+public class Generic {
+    public static void main(String[] args) {
+        MyGen <Integer>myGen = new MyGen<>(12, "Sai Swarup Generics");
+        Integer a = myGen.getT();
+        System.out.println(a + myGen.getName());
+    }
+}
